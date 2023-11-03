6648b790bc682967983e955b8bc27f46eb5f745c
Vikas Kumar <vvvikask@google.com>
2023-11-03 15:30:25 +0530

    first

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <div className='container-max text-center py-4 border-t'>
+      Hungeryy, made by{' '}
+      <a
+        href='https://www.linkedin.com/in/vikas-kumar-95a1341a9/'
+        target={'_blank'}
+        className='text-orange-500'
+      >
+        Vikas Kumar
+      </a>
+    </div>
+  );
+};
+export default Footer;
